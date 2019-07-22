df8a06338a55ac989d72dcc42922817770147661
sorgelig <pour.garbage@gmail.com>
2019-07-23 07:29:35 +0800

    ddram: more clean output.

--- ddram.sv
+++ ddram.sv
@@ -40,15 +40,15 @@ module ddram
 	input         we_req,
 	output reg    we_ack,
 
-	input  [27:0] rdaddr,
-	output  [7:0] dout,
-	input         rd_req,
-	output reg    rd_ack,
+	input     [27:0] rdaddr,
+	output reg [7:0] dout,
+	input            rd_req,
+	output reg       rd_ack,
 	
-	input  [27:0] rdaddr2,
-	output  [7:0] dout2,
-	input         rd_req2,
-	output reg    rd_ack2 
+	input     [27:0] rdaddr2,
+	output reg [7:0] dout2,
+	input            rd_req2,
+	output reg       rd_ack2 
 );
 
 assign DDRAM_BURSTCNT = ram_burst;
@@ -57,9 +57,6 @@ assign DDRAM_ADDR     = {4'b0011, ram_address[27:3]}; // RAM at 0x30000000
 assign DDRAM_RD       = ram_read;
 assign DDRAM_DIN      = ram_data;
 assign DDRAM_WE       = ram_write;
-
-assign dout  = ram_q [{rdaddr[2:0],  3'b000} +:8];
-assign dout2 = ram_q2[{rdaddr2[2:0], 3'b000} +:8]; 
 
 reg  [7:0] ram_burst;
 reg [63:0] ram_q, next_q, ram_q2, next_q2;
@@ -86,10 +83,14 @@ always @(posedge DDRAM_CLK) begin
 					state       <= 1;
 				end
 				else if(rd_req != rd_ack) begin
-					if(cache_addr[27:3] == rdaddr[27:3]) rd_ack <= rd_req;
+					if(cache_addr[27:3] == rdaddr[27:3]) begin
+						rd_ack      <= rd_req;
+						dout        <= ram_q[{rdaddr[2:0],3'b000} +:8];
+					end
 					else if((cache_addr[27:3]+1'd1) == rdaddr[27:3]) begin
 						rd_ack      <= rd_req;
 						ram_q       <= next_q;
+						dout        <= next_q[{rdaddr[2:0],3'b000} +:8];
 						cache_addr  <= {rdaddr[27:3],3'b000};
 						ram_address <= {rdaddr[27:3]+1'd1,3'b000};
 						ram_read    <= 1;
@@ -107,10 +108,14 @@ always @(posedge DDRAM_CLK) begin
 					end 
 				end
 				else if(rd_req2 != rd_ack2) begin
-					if(cache_addr2[27:3] == rdaddr2[27:3]) rd_ack2 <= rd_req2;
+					if(cache_addr2[27:3] == rdaddr2[27:3]) begin
+						rd_ack2     <= rd_req2;
+						dout2       <= ram_q2[{rdaddr2[2:0],3'b000} +:8];
+					end
 					else if((cache_addr2[27:3]+1'd1) == rdaddr2[27:3]) begin
 						rd_ack2     <= rd_req2;
 						ram_q2      <= next_q2;
+						dout2       <= next_q2[{rdaddr2[2:0],3'b000} +:8];
 						cache_addr2 <= {rdaddr2[27:3],3'b000};
 						ram_address <= {rdaddr2[27:3]+1'd1,3'b000};
 						ram_read    <= 1;
@@ -140,10 +145,12 @@ always @(posedge DDRAM_CLK) begin
 			2: if(DDRAM_DOUT_READY) begin
 					if (~ch) begin
 						ram_q  <= DDRAM_DOUT;
+						dout   <= DDRAM_DOUT[{rdaddr[2:0],  3'b000} +:8];
 						rd_ack <= rd_req;
 					end
 					else begin
 						ram_q2  <= DDRAM_DOUT;
+						dout2   <= DDRAM_DOUT[{rdaddr2[2:0],3'b000} +:8];
 						rd_ack2 <= rd_req2;
 					end 
 					state  <= 3;
