0c6ca68a0711f71dfbeac22571834ae092fc72e0
sorgelig <pour.garbage@gmail.com>
2019-07-25 00:30:55 +0800

    Switch to T80.

--- cpu/cpu_z80.v
+++ cpu/cpu_z80.v
@@ -22,66 +22,30 @@ module cpu_z80(
 	input [7:0] SDD_IN,
 	output [7:0] SDD_OUT,
 	output [15:0] SDA,
-	output reg nIORQ, nMREQ,
+	output reg nIORQ,
+	output nMREQ,
 	output reg nRD, nWR,
-	input nINT, nNMI
+	input nINT, nNMI, nWAIT
 );
 
-	reg [7:0] SDD_IN_REG;
-	
-	wire [6:0] T_STATE;
-	wire [6:0] M_CYCLE;
-	wire nINTCYCLE;
-	wire NO_READ;
-	wire WRITE;
-	wire IORQ;
+	wire RFSH_n, MREQ_n;
+	assign nMREQ = MREQ_n | ~RFSH_n;
 
-	tv80_core TV80( , IORQ, NO_READ, WRITE, , , , SDA, SDD_OUT, M_CYCLE,
-							T_STATE, nINTCYCLE, , , nRESET, CLK_4M, 1'b1, 1'b1,
-							nINT, nNMI, 1'b1, SDD_IN, SDD_IN_REG);
-	
-	always @(posedge CLK_4M)
-	begin
-		if (!nRESET)
-		begin
-			nRD <= #1 1'b1;
-			nWR <= #1 1'b1;
-			nIORQ <= #1 1'b1;
-			nMREQ <= #1 1'b1;
-			SDD_IN_REG <= #1 8'b00000000;
-		end
-		else
-		begin
-			nRD <= #1 1'b1;
-			nWR <= #1 1'b1;
-			nIORQ <= #1 1'b1;
-			nMREQ <= #1 1'b1;
-			if (M_CYCLE[0])
-			begin
-				if (T_STATE[1])
-				begin
-					nRD <= #1 ~nINTCYCLE;
-					nMREQ <= #1 ~nINTCYCLE;
-					nIORQ <= #1 nINTCYCLE;
-				end
-			end
-			else
-			begin
-				if ((T_STATE[1]) && NO_READ == 1'b0 && WRITE == 1'b0)
-				begin
-					nRD <= #1 1'b0;
-					nIORQ <= #1 ~IORQ;
-					nMREQ <= #1 IORQ;
-				end
-				if ((T_STATE[1]) && WRITE == 1'b1)
-				begin
-					nWR <= #1 1'b0;
-					nIORQ <= #1 ~IORQ;
-					nMREQ <= #1 IORQ;
-				end
-			end
-			if (T_STATE[2]) SDD_IN_REG <= #1 SDD_IN;
-		end
-	end
-	
+	T80s cpu(
+		.RESET_n(nRESET),
+		.CLK(CLK_4M),
+		.CEN(1),
+		.WAIT_n(nWAIT),
+		.INT_n(nINT),
+		.NMI_n(nNMI),
+		.MREQ_n(MREQ_n),
+		.IORQ_n(nIORQ),
+		.RD_n(nRD),
+		.WR_n(nWR),
+		.RFSH_n(RFSH_n),
+		.A(SDA),
+		.DI(SDD_IN),
+		.DO(SDD_OUT)
+	);
+
 endmodule
