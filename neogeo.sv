6035ed3f8485240b59df65b0793b0d8f085c5e8a
sorgelig <pour.garbage@gmail.com>
2020-06-22 23:32:15 +0800

    Update sys.

--- neogeo.sv
+++ neogeo.sv
@@ -82,6 +82,7 @@ module emu
 	// b[0]: osd button
 	output  [1:0] BUTTONS,
 
+	input         CLK_AUDIO, // 24.576 MHz
 	output [15:0] AUDIO_L,
 	output [15:0] AUDIO_R,
 	output        AUDIO_S,   // 1 - signed audio samples, 0 - unsigned
@@ -352,6 +353,7 @@ hps_io #(.STRLEN($size(CONF_STR)>>3), .WIDE(1), .VDNUM(2)) hps_io
 (
 	.clk_sys(clk_sys),
 	.HPS_BUS(HPS_BUS),
+	.EXT_BUS(),
 
 	.conf_str(CONF_STR),
 	.forced_scandoubler(forced_scandoubler),
