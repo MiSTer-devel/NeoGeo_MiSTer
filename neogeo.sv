7718556bb75f13736452b6877cc85f1633079d9c
sorgelig <pour.garbage@gmail.com>
2021-02-25 23:06:20 +0800

    Update sys.

--- neogeo.sv
+++ neogeo.sv
@@ -1777,16 +1777,8 @@ video_cleaner video_cleaner
 video_mixer #(.LINE_LENGTH(320), .HALF_DEPTH(0), .GAMMA(1)) video_mixer
 (
 	.*,
-
-	.clk_vid(CLK_VIDEO),
-	.ce_pix(ce_pix),
-	.ce_pix_out(CE_PIXEL),
-
-	.scanlines(0),
 	.scandoubler(scale || forced_scandoubler),
 	.hq2x(scale==1),
-
-	.mono(0),
 
 	.VGA_DE(vga_de),
 	.R(r),
