f2afca637ba8f875171477f9b6650a7bb2d67aa0
Sorgelig <pour.garbage@gmail.com>
2022-01-05 23:19:56 +0800

    Update sys.

--- neogeo.sv
+++ neogeo.sv
@@ -46,7 +46,7 @@ module emu
 	input         RESET,
 
 	//Must be passed to hps_io module
-	inout  [45:0] HPS_BUS,
+	inout  [47:0] HPS_BUS,
 
 	//Base video clock. Usually equals to CLK_SYS.
 	output        CLK_VIDEO,
