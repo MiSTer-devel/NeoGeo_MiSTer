83e20c83ea5a5000fbb8a72f26e056d9f169c573
Sorgelig <pour.garbage@gmail.com>
2022-11-10 00:03:30 +0800

    Update sys.

--- neogeo.sv
+++ neogeo.sv
@@ -69,13 +69,14 @@ module emu
 	output        VGA_F1,
 	output [1:0]  VGA_SL,
 	output        VGA_SCALER, // Force VGA scaler
+	output        VGA_DISABLE, // analog out is off
 
 	input  [11:0] HDMI_WIDTH,
 	input  [11:0] HDMI_HEIGHT,
 	output        HDMI_FREEZE,
 
 `ifdef MISTER_FB
-	// Use framebuffer in DDRAM (USE_FB=1 in qsf)
+	// Use framebuffer in DDRAM
 	// FB_FORMAT:
 	//    [2:0] : 011=8bpp(palette) 100=16bpp 101=24bpp 110=32bpp
 	//    [3]   : 0=16bits 565 1=16bits 1555
@@ -205,6 +206,7 @@ assign LED_DISK  = 0;
 assign LED_POWER = 0;
 assign BUTTONS   = osd_btn;
 assign VGA_SCALER= 0;
+assign VGA_DISABLE = 0;
 assign HDMI_FREEZE = 0;
 
 wire [1:0] ar       = status[33:32];
@@ -309,7 +311,7 @@ localparam CONF_STR = {
 	"P1-;",
 	"P1O56,Stereo Mix,none,25%,50%,100%;",
 	"P1-;",
-	"-;",
+	"- ;",
 	"RE,Reset & apply;",  // decouple manual reset from system reset 
 	"J1,A,B,C,D,Start,Select,Coin,ABC;",	// ABC is a special key to press A+B+C at once, useful for keyboards that don't allow more than 2 keypresses at once
 	"jn,A,B,X,Y,Start,Select,L,R;",	        // name mapping 
