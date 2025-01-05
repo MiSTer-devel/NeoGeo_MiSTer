5b546ae6b76f20ac2253bf5b0c61d4b733aa28f2
Sorgelig <pour.garbage@gmail.com>
2025-01-05 13:14:59 +0800

    Update sys.

--- neogeo.sv
+++ neogeo.sv
@@ -74,6 +74,7 @@ module emu
 	input  [11:0] HDMI_WIDTH,
 	input  [11:0] HDMI_HEIGHT,
 	output        HDMI_FREEZE,
+	output        HDMI_BLACKOUT,
 
 `ifdef MISTER_FB
 	// Use framebuffer in DDRAM
@@ -208,6 +209,7 @@ assign BUTTONS   = osd_btn;
 assign VGA_SCALER= 0;
 assign VGA_DISABLE = 0;
 assign HDMI_FREEZE = 0;
+assign HDMI_BLACKOUT = 0;
 
 wire [1:0] ar       = status[33:32];
 wire       vcrop_en = status[34];
